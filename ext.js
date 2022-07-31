1bf9da6dd1fe960a7e40f582dd199eec55c553e6
DresdenE <57573780+DresdenE@users.noreply.github.com>
2022-07-30 20:51:06 -0500

    Simplify code

--- ext.js
+++ ext.js
@@ -15,16 +15,7 @@ function onLoaded () {
     csInterface.evalScript("$._PPP_.getVersionInfo()", myVersionInfoFunction);    
     csInterface.evalScript("$._PPP_.getActiveSequenceName()", myCallBackFunction);        
     csInterface.evalScript("$._PPP_.keepPanelLoaded()");
-    csInterface.evalScript("$._PPP_.disableImportWorkspaceWithProjects()");
-    csInterface.evalScript("$._PPP_.registerProjectPanelSelectionChangedFxn()");      // Project panel selection changed
-    csInterface.evalScript("$._PPP_.registerItemAddedFxn()");                          // Item added to project
-    csInterface.evalScript("$._PPP_.registerProjectChangedFxn()");                    // Project changed
-    csInterface.evalScript("$._PPP_.registerSequenceSelectionChangedFxn()");        // Selection within the active sequence changed
-    csInterface.evalScript("$._PPP_.registerSequenceActivatedFxn()");                // The active sequence changed
-    csInterface.evalScript("$._PPP_.registerActiveSequenceStructureChangedFxn()");    // Clips within the active sequence changed
-    csInterface.evalScript("$._PPP_.registerItemsAddedToProjectFxn()");  // register for message, whenever something is added to the active project
-    csInterface.evalScript("$._PPP_.registerSequenceMessaging()");            
-    csInterface.evalScript("$._PPP_.registerActiveSequenceChangedFxn()");    
+    csInterface.evalScript("$._PPP_.disableImportWorkspaceWithProjects()");  
     csInterface.evalScript("$._PPP_.confirmPProHostVersion()");
     csInterface.evalScript("$._PPP_.forceLogfilesOn()");  // turn on log files when launching
 
